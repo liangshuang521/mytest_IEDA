cc37a2346c16c6f78e8586ac76d00ab535962015
liangshuang <13025432953@163.com>
2019-12-28 20:22:03 +0800

    主线提交

---
+++ src/main/java/com/liantongkeji/domain/A.java
@@ -0,0 +1,9 @@
+package com.liantongkeji.domain;
+
+/**
+ * @Auther: LiangShuang
+ * @Date: 2019/12/28 20:20
+ * @Description:
+ */
+public class A {
+}
---
+++ src/main/java/com/liantongkeji/domain/B.java
@@ -0,0 +1,9 @@
+package com.liantongkeji.domain;
+
+/**
+ * @Auther: LiangShuang
+ * @Date: 2019/12/28 20:21
+ * @Description:
+ */
+public class B {
+}
